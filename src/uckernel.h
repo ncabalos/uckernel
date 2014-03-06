e4c1fcd23cff1aec2537eb42dd3f5063ca9463d3
Nathaniel Abalos <nathaniel_abalos@hotmail.com>
2014-03-05 23:16:25 -0600

    Added some comments

--- src/uckernel.h
+++ src/uckernel.h
@@ -15,13 +15,26 @@ typedef struct uckernel_event_s {
     void * data;
 } uckernel_event;
 
+/**
+ * This initializes the uckernel and sets up the required resources. The system
+ * tick timer must be initialized before this function is called.
+ * 
+ * @return true = success; false = failed
+ */
 bool uckernel_init(void);
+/**
+ * This should be called every iteration of the main loop to allow the uckernel
+ * to process events.
+ */
 void uckernel_event_loop(void);
 bool uckernel_post_event(const uckernel_task func, uint16_t event, void * data,
                          uint16_t delay_ms);
 bool uckernel_task_register(char * task_name, uckernel_task func,
                             uckernel_event * event_queue,
                             uint16_t event_queue_size, uint16_t priority);
+/**
+ * This should be called every tick event to process posted delayed events.
+ */
 void uckernel_tick_handler(void);
 
 #endif	/* UCKERNEL_H */
